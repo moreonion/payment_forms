2eef5e861bc5d4705d71ac3d61136e76255ffd77
Roman Zimmermann <roman@more-onion.com>
2022-03-12 10:57:59 +0100

    Bump expiry date for valid visa test card

--- tests/CreditCardFormTest.php
+++ tests/CreditCardFormTest.php
@@ -10,7 +10,7 @@ class CreditCardFormTest extends \DrupalUnitTestCase {
       'secure_code' => '804',
       'expiry_date' => array(
         'month' => '06',
-        'year' => '2016',
+        'year' => '2080',
       ),
     )
   );
@@ -37,7 +37,7 @@ class CreditCardFormTest extends \DrupalUnitTestCase {
     $elements = $this->elements();
     $form->validateValues($elements, $data);
     $this->assertInstanceOf('DateTime', $data['expiry_date']);
-    $this->assertEqual('2016-06', $data['expiry_date']->format('Y-m'));
+    $this->assertEqual('2080-06', $data['expiry_date']->format('Y-m'));
   }
 
   function testValidation_validVisaWithWhitespace() {
